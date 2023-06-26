fc930d1b40d3ad5926d4ce05477f8c0014b82c45
Sadan Mian <sadanmiyan007@gmail.com>
2023-06-26 13:01:59 +0530

    String in js

---
+++ 13_Strings_in_JS/index.js
@@ -0,0 +1,25 @@
+let namee = "Sadan"
+console.log(namee);
+
+let naam = 'Abdul'
+console.log(naam);
+
+// Template Literals
+
+let boy1 = "Abdul"
+let boy2 = "Sadan"
+
+let sentance = `${boy1} is friend of ${boy2}`
+console.log(sentance);
+
+// Escape Sequence Characters
+let fruit = 'Bana\'na'
+let fruit2 = "App\"le"
+let fruit3 = 'Papa\nya'
+let fruit4 = 'App\tle'
+let fruit5 = 'App\rle'
+console.log(fruit);
+console.log(fruit2);
+console.log(fruit3);
+console.log(fruit4);
+console.log(fruit5);
