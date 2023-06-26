4a330751fd9dd60829e5864ad0d85f36431b845f
Sadan Mian <sadanmiyan007@gmail.com>
2023-06-26 21:45:01 +0530

    Map Filter Reduce

---
+++ 20_Map_Filter_Reduce/index.js
@@ -0,0 +1,25 @@
+// let arr = [45, 23, 21]
+
+// // Map 
+// console.log(arr);
+
+// let a = arr.map((value, index, array) => {
+//     console.log(value, index, array);
+//     return value + 1;
+// })
+
+// console.log(a);
+
+// // Array filter Method
+// let arr2 = [45, 23, 21, 0, 3, 5]
+// let a2 = arr2.filter(value => {
+//     return value < 10;
+// })
+// console.log(a2);
+
+// Reduce Method
+let arr3 = [1, 2, 3, 5, 2, 1]
+let a3 = arr3.reduce((x, y) => {
+    return x + y;
+})
+console.log(a3);
