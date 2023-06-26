3d4f18943d896bb8d09c137f4411f81f49b9dfdb
Sadan Mian <sadanmiyan007@gmail.com>
2023-06-26 20:50:52 +0530

    Loops in Arrays

---
+++ 19_Loops_In_JS/index.js
@@ -0,0 +1,29 @@
+let num = [1, 53, 43, 593, 22, 917, 92]
+
+// Loop
+// for (let i = 0; i < num.length; i++) {
+//     console.log(num[i]);
+// }
+
+// For Each Loop
+// num.forEach(element => {
+//     console.log(element);
+// });
+// num.forEach(element => {
+//     console.log(element*element);
+// });
+
+// Array.from
+// let namee = "Sadan"
+// let aar = Array.from(namee)
+// console.log(aar);
+
+// for...of
+// for (let i of num) {
+//     console.log(i);
+// }
+
+// for...in
+for (let i in num) {
+    console.log(i);
+}
