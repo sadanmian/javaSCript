c9dd691717afaa2f0408b03321665c89273aa0d3
Sadan Mian <sadanmiyan007@gmail.com>
2023-06-26 12:10:21 +0530

    Functions in JavaScript

---
+++ 11_Function_in_JS/index.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+    console.log("sum of ", a, " and ", b, " is = ", a + b);
+}
+
+add(1, 3)
+
+const sum = (x, y) => {
+    return x + y;
+}
+
+console.log(sum(5,10));
+
+const hello = ()=>{
+    console.log("Print Hello");
+}
+
+hello()
