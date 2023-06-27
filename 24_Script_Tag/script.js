c0c402ec47307e7314c621d264844b29889b6d1a
Sadan Mian <sadanmiyan007@gmail.com>
2023-06-27 11:02:55 +0530

    Script Tag

---
+++ 24_Script_Tag/script.js
@@ -0,0 +1,7 @@
+console.log("This is Sadan Mian");
+
+let num1 = prompt("Enter First Number")
+let num2 = prompt("Enter Second Number")
+let sum = Number.parseInt(num1) + Number.parseInt(num2)
+
+alert(`The Sum of ${num1} and ${num2} is ${sum}`)
