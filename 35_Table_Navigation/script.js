c9939fc002c21474012ed8338fca8782ed4807c3
Sadan Mian <sadanmiyan007@gmail.com>
2023-06-30 00:39:54 +0530

    Table Navigation

---
+++ 35_Table_Navigation/script.js
@@ -0,0 +1,4 @@
+console.log(document.body.firstElementChild.firstElementChild.rows);
+console.log(document.body.firstElementChild.firstElementChild.caption);
+console.log(document.body.firstElementChild.firstElementChild.tHead.firstElementChild);
+console.log(document.body.firstElementChild.firstElementChild.tBodies);
