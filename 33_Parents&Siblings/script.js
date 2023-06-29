9b4c2b003ab61f7158c7c435538706f6e32e9547
Sadan Mian <sadanmiyan007@gmail.com>
2023-06-29 23:43:05 +0530

    Parents & Siblings

---
+++ 33_Parents&Siblings/script.js
@@ -0,0 +1,4 @@
+console.log(document.body.firstChild);
+console.log(document.body.firstChild.parentNode);
+console.log(document.body.firstChild.parentElement);
+console.log(document.body.firstChild.firstChild.nextSibling);
