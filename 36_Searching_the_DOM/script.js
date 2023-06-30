5180a4e6df999894d6a93523f0c9fbb3eb23348c
Sadan Mian <sadanmiyan007@gmail.com>
2023-06-30 15:43:32 +0530

    Searching the DOM

---
+++ 36_Searching_the_DOM/script.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName("card-title")[0].style.color = "red";
+
+console.log(document.querySelectorAll(".card-title"));
+
+document.querySelectorAll(".card-title")[0].style.color = "blue"
+document.querySelectorAll(".card-title")[1].style.color = "red"
+document.querySelectorAll(".card-title")[2].style.color = "green"
+
+document.querySelector('.card-title').style.background = "pink"
