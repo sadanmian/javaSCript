f39dbf28a33f1c69a2e27d3cdf6fade03610a3dc
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-01 10:59:40 +0530

    innerHTML, outerHTML and other properties

---
+++ 41_innerHTML_outerHTML/script.js
@@ -0,0 +1,8 @@
+let x = document.getElementsByTagName('span')[0]
+console.log(x);
+console.dir(x)
+console.log(document.body.firstChild.nodeName);
+console.log(document.body.firstElementChild.nodeName);
+console.log(first.innerHTML);
+console.log(first.outerHTML);
+console.log(document.body.textContent);
