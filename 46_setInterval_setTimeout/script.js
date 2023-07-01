0b1b219bcb2aae211e8e2a283b5b6e3b8567ec4a
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-01 13:38:30 +0530

    setInterval and setTimeout

---
+++ 46_setInterval_setTimeout/script.js
@@ -0,0 +1,24 @@
+// document.write("Hello")
+
+// setTimeout(() => {
+//     alert("This is Set TimeOut")
+//     document.write("This is Set TimeOut using document.write")
+// }, 2000)
+
+// let x = 0;
+// setInterval(() => {
+//     x++;
+//     document.write(x)
+// }, 1000)
+
+// const first = () => {
+//     document.body.style.background = "red"
+// }
+// const second = () => {
+//     document.body.style.background = "green"
+// }
+// const all = () => {
+//     setInterval(first, 500)
+//     setInterval(second, 500)
+// }
+// setInterval(all, 1000)
