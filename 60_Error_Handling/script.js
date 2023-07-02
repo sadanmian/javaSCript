bb1c1d70f9002c1b7542fa139be26b61f4ef4ce0
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-02 20:43:26 +0530

    Error Handling: try and catch

---
+++ 60_Error_Handling/script.js
@@ -0,0 +1,21 @@
+setTimeout(() => {
+    console.log("Hacking wifi....Please Wait.....");
+}, 1000);
+
+try {
+    console.log(sadan);
+} catch (error) {
+    console.log(error);
+}
+
+setTimeout(() => {
+    console.log("Fetching Username and Password.....Please Wait....");
+}, 2000);
+
+setTimeout(() => {
+    console.log("Hacking Sadan's Facebook id");
+}, 3000);
+
+setTimeout(() => {
+    console.log("Username and Password of Sadan (7409907246) fetched Please Wait...");
+}, 4000);
