b2ed0c3d64f47fa4699de5111958f41e06b4bf69
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-03 10:58:19 +0530

    The Finally Clause

---
+++ 62_Finally_Clause/script.js
@@ -0,0 +1,13 @@
+try {
+    let a = 0
+    log = log
+    console.log("Program Run Successfully");
+} catch (error) {
+    log = log
+    console.log("This is an Error");
+}
+
+finally {
+    console.log("This is run due to finally");
+}
+// console.log("This is a good boy");
