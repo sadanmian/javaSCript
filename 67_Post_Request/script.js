f5b9e851580073505ea6e3e607425d78ced85a6f
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-03 15:49:18 +0530

    Sending POST request with Fetch API

---
+++ 67_Post_Request/script.js
@@ -0,0 +1,28 @@
+const createTodo = async (todo) => {
+    let options = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(todo)
+    }
+    let p = await fetch('https://jsonplaceholder.typicode.com/posts', options)
+    let response = await p.json()
+    return response
+}
+const getTodo = async (id) => {
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id)
+    let r = response.json()
+    return r
+}
+const mainFunc = async () => {
+    let todo = {
+        title: 'Sadan',
+        body: 'Mian',
+        userId: 12,
+    }
+    let todor = await createTodo(todo)
+    console.log(todor);
+    console.log(await getTodo(5));
+}
+mainFunc()
