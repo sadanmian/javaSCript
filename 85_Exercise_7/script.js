fa6349bf820a0783bd590a19a8c9fb0eb352da5c
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-04 23:17:15 +0530

    Password Generator

--- 85_Exercise_7/script.js
+++ 85_Exercise_7/script.js
@@ -1,1 +1,4 @@
 // Password Generator
+
+// Create a javaScript program capable of generating a password which contains atleast one lowercase, one uppercase and one special character. 
+// Create a password class to acheive the same
